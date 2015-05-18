e7545d610eb582ce7fe0a6f20700c36db1a8a584
Evelyn S <elxsham@hotmail.com>
2015-05-17 23:28:25 -0400

    Modified BlackjackVariation.py - v0.2

--- BlackjackVariation/BlackjackVariation.py
+++ BlackjackVariation/BlackjackVariation.py
@@ -12,11 +12,11 @@
 
 import random
 
-#initialzing game variables
-gameScore = 100
-roundScore = 0
-round = 1   # round number
-deck = []   # cards per suit in deck
+# initializing game variables
+game_score = 100
+round_score = 0
+round_num = 1  # round number
+deck = []  # cards per suit in deck
 drawn = []  # cards drawn during game
 
 # function to build card deck - 4 cards per suit
@@ -24,58 +24,72 @@ drawn = []  # cards drawn during game
 # using indices will cause an "out of range" index error
 def rebuild():
     for i in range(1, 14):
-        deck.append(4)		
-        
+        deck.append(4)
+
 # DEBUGGING        
 # for num in deck:
 #     print(num)
 
-# choosing random cards from deck
-# input number of times a random card is drawn
-# records which cards are drawn and sets point values
-# removes card from deck for each card drawn
-def drawCard(times):
-    
-    for i in range(1, (times + 1)):
-        card = random.randint(1, 13)   # picking suit
-        
-        # insert different points based on card drawn
-        # if a face card, set point value to 10
-        if 9 < card < 14:    
-            drawn.append(10)
-            
-            
-        
-        # for any other card, set value to card suit number
-        else:
-            drawn.append(card)
-
-# NEED TO FIX!
-
-# faceCards = { 11 : jack,
-#     12 : queen,
-#     13 : king,
-# }
-
-# def jack():
-#     print("You drew a jack.\n")
-    
-# def queen():
-#     print("You drew a queen.\n")
-    
-# def king():
-#     print("You drew a king.\n")    
-    
-# faceCardss[11]()                
-
 
 # GAME STARTS - lasts for 5 rounds
 while round < 6:
-    
+
     # printing round number
-    print("----- ROUND %d ------" % round)
-    
-    
-        
-    
+    print("----- ROUND %d ------\n\n" % round)
+
+    # choosing random cards from deck
+    # input number of times a random card is drawn
+    # records which cards are drawn and sets point values
+    # removes card from deck for each card drawn
+    def draw_card(times):
+
+        # tells player the face card drawn
+        def face_card(n):
+            if n == 11:
+                print("You draw a JACK.\n")
+
+            elif n == 12:
+                print("You draw a QUEEN.\n")
+
+            elif n == 13:
+                print("You draw a KING.\n")
+
+            # drawing card(s)
+            for i in range(1, (times + 1)):
+                print("You draw a card.\n")
+
+                card = random.randint(1, 13)  # picking card value - the card drawn
+
+                # checks if card value has cards left, else draws value again until a card can be drawn
+                while deck[card] == 0:
+                    card = random.randint(1, 13)
+
+                deck[card] -= 1     # removing quantity left of card value drawn from deck
+
+                # insert different points based on card drawn
+                # if a face card, set point value to 10
+                if 9 < card < 14:
+                    drawn.append(10)
+                    face_card(card)  # tell player which face card has been drawn
+
+                # for any other card, set value to card suit number
+                else:
+                    drawn.append(card)
+                    print("You draw a %d.\n" % card)
+
+    draw_card(2)     # drawing two initial cards
+
+
+    # checking card value scores to see if user has bust or can redraw
+    def check_score():
+        if sum(drawn) >= 21:  # if user has bust, round ends
+            round += 1
+
+        input("Draw again? (Y/N)")
+
+    check_score()
+
+
+
+
 
