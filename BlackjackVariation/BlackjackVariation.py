70748165c684e389db5eb7a70567b122ad4e85a7
Evelyn S <elxsham@hotmail.com>
2015-05-18 14:47:03 -0400

    Modified BlackjackVariation - v0.3

--- BlackjackVariation/BlackjackVariation.py
+++ BlackjackVariation/BlackjackVariation.py
@@ -1,10 +1,11 @@
 # BLACKJACK VARIATION - 1 PLAYER
 # Game score will begin at 100, and the game will last for five rounds
-# At the beginning of the round, the player is given two random cards from a deck and they will be added together to make the player's round score 
+# At the beginning of the round, the player is given two random cards from a deck and they will be added together to make the round score
 # Player can choose to draw another card to try to get their round score closer to 21, or they can end the round 
 # The player can draw as many cards as they want until they end the round or their round score exceeds 21.
 # At the end of the round, the difference between 21 and the round score is subtracted from the game score, and then the next round begins 
 # After the five rounds, the player is given their total score and the game is over.
+# If player busts, 21 is subtracted from their total score.
 
 # ---- CARD VALUES ----
 # Ace = 1
@@ -12,10 +13,7 @@
 
 import random
 
-# initializing game variables
-game_score = 100
-round_score = 0
-round_num = 1  # round number
+# initializing game lists
 deck = []  # cards per suit in deck
 drawn = []  # cards drawn during game
 
@@ -30,64 +28,96 @@ def rebuild():
 # for num in deck:
 #     print(num)
 
+# choosing random cards from deck
+# input number of times a random card is drawn
+# records which cards are drawn and sets point values
+# removes card from deck for each card drawn
+def draw_card(times):
 
-# GAME STARTS - lasts for 5 rounds
-while round < 6:
+    # tells player the face card drawn
+    def face_card(n):
+        if n == 11:
+            print("You draw a JACK.\n")
 
-    # printing round number
-    print("----- ROUND %d ------\n\n" % round)
+        elif n == 12:
+            print("You draw a QUEEN.\n")
 
-    # choosing random cards from deck
-    # input number of times a random card is drawn
-    # records which cards are drawn and sets point values
-    # removes card from deck for each card drawn
-    def draw_card(times):
+        elif n == 13:
+            print("You draw a KING.\n")
 
-        # tells player the face card drawn
-        def face_card(n):
-            if n == 11:
-                print("You draw a JACK.\n")
+        # drawing card(s)
+        for i in range(1, (times + 1)):
+            print("You draw a card.\n")
 
-            elif n == 12:
-                print("You draw a QUEEN.\n")
+            card = random.randint(1, 13)  # picking card value - the card drawn
 
-            elif n == 13:
-                print("You draw a KING.\n")
+            # checks if card value has cards left, else draws value again until a card can be drawn
+            while deck[card] == 0:
+                card = random.randint(1, 13)
 
-            # drawing card(s)
-            for i in range(1, (times + 1)):
-                print("You draw a card.\n")
+            deck[card] -= 1     # removing quantity left of card value drawn from deck
 
-                card = random.randint(1, 13)  # picking card value - the card drawn
+            # insert different points based on card drawn
+            # if a face card, set point value to 10
+            if 9 < card < 14:
+                drawn.append(10)
+                face_card(card)  # tell player which face card has been drawn
 
-                # checks if card value has cards left, else draws value again until a card can be drawn
-                while deck[card] == 0:
-                    card = random.randint(1, 13)
+            # for any other card, set value to card suit number
+            else:
+                drawn.append(card)
+                print("You draw a %d.\n" % card)
 
-                deck[card] -= 1     # removing quantity left of card value drawn from deck
+# checking card value scores to see if user has bust or can redraw
+def check_score():
+    total = sum(drawn)
+    if total >= 21:  # if user has bust, round ends
+        return -1
 
-                # insert different points based on card drawn
-                # if a face card, set point value to 10
-                if 9 < card < 14:
-                    drawn.append(10)
-                    face_card(card)  # tell player which face card has been drawn
+    else:
+        ans = input("Draw again? (Y/N)")
+        if ans == 'N':
+            return total
+        else:
+            while ans == 'Y':
+                draw_card(1)
+                check_score()
 
-                # for any other card, set value to card suit number
-                else:
-                    drawn.append(card)
-                    print("You draw a %d.\n" % card)
+# main function
+def main():
+    # initializing game variables
+    game_score = 100
+    round_num = 1  # initializing round number
 
-    draw_card(2)     # drawing two initial cards
+    while round_num < 6:    # game lasts for 5 rounds
+        print("----- ROUND %d -----\n\n" % round_num)     # printing round number
 
+        draw_card(2)     # drawing two initial cards
 
-    # checking card value scores to see if user has bust or can redraw
-    def check_score():
-        if sum(drawn) >= 21:  # if user has bust, round ends
-            round += 1
+        if check_score() == -1:  # checking user score and round status
+            game_score -= 21    # getting game score
+            print("You have bust!\n|SCORE|\nRound Score: 21\nGame Score: %d\n" % game_score)
+        else:
+            round_score = check_score() # getting round score
+            game_score -= (21 - round_score)    # getting game score
 
-        input("Draw again? (Y/N)")
+            print("You decide not to draw any more cards.\n")
+            print("|SCORE|\nRound Score: %d\nGame Score: %d\n" % (round_score, game_score))
 
-    check_score()
+        round_num += 1  # go to next round
+
+        if round_num == 5:
+            print("Let's move onto the final round.\n\n")
+        else:
+            print("Let's move onto the next round.\n\n")
+        continue
+
+    print("GAME OVER!\nFINAL SCORE: %d" % game_score)
+
+# calling main()
+if __name__ == "__main__":
+    main()
+
 
 
 
