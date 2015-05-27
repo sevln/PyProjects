39edc1abd3c494361e9614fe62d18a674365a375
Evelyn S <elxsham@hotmail.com>
2015-05-26 23:35:14 -0400

    Initial commit of CountdownClock.py Added comments outlining program

---
+++ CountdownClock/CountdownClock.py
@@ -0,0 +1,9 @@
+# COUNTDOWN CLOCK
+# User to inputs time and date
+# Program will print out a message at given intervals, telling user how much longer there is until the selected time
+
+# SUBGOALS
+# If the selected time has already passed, have the program tell the user to start over.
+# Asks for the year, month, day, hour separately, allow the user to type in either the month name or its number.
+# Use of built in modules such as time and datetime can change this project from a nightmare into a much simpler task.
+
