16490335308b0077a9a143d5c59e389bdd58b134
Evelyn S <elxsham@hotmail.com>
2015-05-27 22:54:44 -0400

    Modified CountdownClock.py Added preliminary function definitions and outline of inputs required from user.

--- CountdownClock/CountdownClock.py
+++ CountdownClock/CountdownClock.py
@@ -7,3 +7,33 @@
 # Asks for the year, month, day, hour separately, allow the user to type in either the month name or its number.
 # Use of built in modules such as time and datetime can change this project from a nightmare into a much simpler task.
 
+import time
+import datetime
+
+# executes and prints countdown
+def countdown():
+
+    return
+
+# gets datetime from user for countdown
+def get_datetime():
+    datetime = []   # datetime list
+
+    print("Enter the starting date and/or time for your countdown...")
+    month = input("Month: ")
+    day = input("Day: ")
+    year = input("Year: ")
+    hour = input("Hour(s): ")
+    minute = input("Minute(s): ")
+
+    return
+
+# main function
+# executes get_datetime function
+def main():
+    get_datetime()
+    return
+
+# calls main function
+if __name__ == "__main__":
+    main()
